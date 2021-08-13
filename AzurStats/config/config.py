ec698013eff0d1a61e14e56d841fd470cdae362e
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2021-08-13 17:34:49 +0800

    Auto create temp folder

--- AzurStats/config/config.py
+++ AzurStats/config/config.py
@@ -1,9 +1,14 @@
+import os
+
 import toml
 
 from module.logger import logger
 
 TEMP_DATA = './AzurStats/data'
 CONFIG_FILE = './AzurStats/config/prod.toml'
+
+if not os.path.exists(TEMP_DATA):
+    os.mkdir(TEMP_DATA)
 
 logger.info(f'Using {CONFIG_FILE}')
 CONFIG = toml.load(CONFIG_FILE)
